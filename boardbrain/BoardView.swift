65647a3c1a07504fc6b523fd4aaaf51b700c417c
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-04-06 12:19:58 +0530

    Creating board views

---
+++ boardbrain/BoardView.swift
@@ -0,0 +1,18 @@
+//
+//  BoardView.swift
+//  boardbrain
+//
+//  Created by Selvarajan on 06/04/24.
+//
+
+import SwiftUI
+
+struct BoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BoardView()
+}
---
+++ boardbrain/Extension/ColorExt.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExt.swift
+//  boardbrain
+//
+//  Created by Selvarajan on 06/04/24.
+//
+
+import Foundation
